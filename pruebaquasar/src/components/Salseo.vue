dbfa37037bbcc0c7d77876cbe67b43e77d2f8abd
dgarcia0 <davidgarcia227@gmail.com>
2019-11-10 18:54:57 +0100

    prueba quasar

---
+++ pruebaquasar/src/components/Salseo.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>Salseo</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'salseo'
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ pruebaquasar/src/components/templates/nuevolayout.vue
@@ -0,0 +1,25 @@
+<template>
+
+  <q-layout>
+    <q-toolbar slot="header">
+
+      <q-toolbar-title>
+        <h1>Titulaco</h1>
+      </q-toolbar-title>
+    </q-toolbar>
+
+    <router-view />
+
+  </q-layout>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {}
+    }
+  }
+</script>
+
+<style>
+</style>
