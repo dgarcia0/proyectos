29e8577cd7a48972fc6a75669c23f99b146127dd
dgarcia0 <davidgarcia227@gmail.com>
2019-11-10 19:02:38 +0100

    pruebas vue

---
+++ pruebas vue/js/service-worker.js
@@ -0,0 +1,25 @@
+let CACHE_NAME = 'cache-prueba';
+let urlsToCache = [
+  '../estilo.css',
+  '../img/1.PNG',
+  '../penjat.html'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request, {ignoreSearch:true}).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
