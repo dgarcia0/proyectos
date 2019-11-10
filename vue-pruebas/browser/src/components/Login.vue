8b994af30fa1e310cf0b615ea20f48065ff64194
dgarcia0 <davidgarcia227@gmail.com>
2019-11-10 18:49:26 +0100

    pruebas vue

---
+++ vue-pruebas/browser/src/components/Login.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>Login</h1>
+    <q-input v-model="text.user" stack-label="Usuario" placeholder="usuario" />
+    <q-input v-model="text.password" float-label="Password" placeholder="password" />
+    <q-btn color="primary" @click="">Submit</q-btn>
+  </div>
+</template>
+
+<script>
+  import { QBtn, QInput } from 'quasar'
+
+  export default {
+    data () {
+      return {
+        text: {
+          user: '',
+          password:''
+        },
+      }
+    },
+    components: {
+      QBtn,
+      QInput
+    },
+    name: 'login'
+  }
+
+</script>
+
+<style scoped>
+
+</style>
